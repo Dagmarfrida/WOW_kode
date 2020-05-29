ead7c8d31585f63a957dd32b84f6862364992ad2
Dagmar Halldorsdottir <dagmarfrida@gmail.com>
2020-05-29 04:03:38 +0200

    oprydning i css/javascript

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ openModalImages.forEach(img => {
     openModal(modal)
     if (modal ='#modalBeer')  {
       initMap();
-      //console.log(modal)
       
       }
       })
@@ -43,9 +42,10 @@ function closeModal(modal) {
   overlay.classList.remove('active')
 }
 
+//kort lavet med hjælp fra https://developers.google.com/maps/documentation/javascript/tutorial
 
 function initMap(){
-    var styledMap = [
+    const styledMap = [
         {
             "featureType": "all",
             "elementType": "all",
@@ -245,25 +245,23 @@ function initMap(){
             ]
         }
     ]
-    //kort lavet med hjælp fra https://developers.google.com/maps/documentation/javascript/tutorial
-        //function initMap(){
     
-            var options = {
-                    zoom: 6,
-                    center:{lat:64.9631, lng:-19.0208},
-                    styles: styledMap
-             }
-            //new map
-            var map = new google.maps.Map(document.getElementById('txtBeer'), options);    
-    
-            //make marker
-            var beer = {
-                url: 'fotos/Beer-PNG-2.png',
-                scaledSize: new google.maps.Size(30, 30)   
-            };
+    const options = {
+        zoom: 6,
+        center:{lat:64.9631, lng:-19.0208},
+        styles: styledMap
+        }
+    //new map
+    const map = new google.maps.Map(document.getElementById('txtBeer'), options);    
+
+    //make marker
+    const beer = {
+        url: 'fotos/Beer-PNG-2.png',
+        scaledSize: new google.maps.Size(30, 30)   
+        };
     
     // marker options
-    var marker = new google.maps.Marker({
+    const marker = new google.maps.Marker({
     positions: map.data.loadGeoJson ('Iceland.geojson'),
     map: map,
     icon: map.data.setStyle({
@@ -273,14 +271,14 @@ function initMap(){
     });
     
      //gets data from geojson
-     var infoWindow = new google.maps.InfoWindow({maxWidth: 350});
+     let infoWindow = new google.maps.InfoWindow({maxWidth: 350});
      map.data.addListener('click', function(event) {
-     var myHTML = event.feature.getProperty("Name");
-     var myDesc = event.feature.getProperty("description");
-     var myAddress = event.feature.getProperty("address");
-     var myTown = event.feature.getProperty("town");
-     var myLink = event.feature.getProperty("link");
-     var myImg = event.feature.getProperty("image");
+     let myHTML = event.feature.getProperty("Name");
+     let myDesc = event.feature.getProperty("description");
+     let myAddress = event.feature.getProperty("address");
+     let myTown = event.feature.getProperty("town");
+     let myLink = event.feature.getProperty("link");
+     let myImg = event.feature.getProperty("image");
      infoWindow.setContent('<div id="iw-container">' +
      '<div class="iw-title">' + myHTML+ '</div>' +
      '<div class="iw-content">' + '<img src="' + myImg +'">' + '<p>' +myDesc+'</div>' + myAddress + '<br>' + myTown +  '<br>' +  myLink + '</div>' +
@@ -292,65 +290,18 @@ function initMap(){
      infoWindow.open(map, marker);
      });
     
-     //info window styling med stor hjælp fra - http://en.marnoto.com/2014/09/5-formas-de-personalizar-infowindow.html
+     //info window - http://en.marnoto.com/2014/09/5-formas-de-personalizar-infowindow.html
     
-     // Event that closes the Info Window with a click on the map
+     //close info window with a click on the map
     google.maps.event.addListener(map, 'click', function() {
         infoWindow.close();
     });
     
-    
-    // START INFOWINDOW CUSTOMIZE.
     // before the opening of the infowindow, defined styles are applied.
     
     google.maps.event.addListener(infoWindow, 'domready', function() {
-    
-    // Reference to the DIV that wraps the bottom of infowindow
-    var iwOuter = $('.gm-style-iw');
-    
-    /* Since this div is in a position prior to .gm-div style-iw.
-    * We use jQuery and create a iwBackground variable,
-    * and took advantage of the existing reference .gm-style-iw for the previous div with .prev().
-    */
-    //var iwBackground = iwOuter.prev();
-    
-    // Removes background shadow DIV
-    //iwBackground.children(':nth-child(2)').css({'display' : 'none'});
-    
-    // Removes white background DIV
-    //iwBackground.children(':nth-child(4)').css({'display' : 'none'});
-    
-    // Moves the infowindow 115px to the right.
-    //iwOuter.parent().parent().css({left: '115px'});
-    
-    // Moves the shadow of the arrow 76px to the left margin.
-    //iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
-    
-    // Moves the arrow 76px to the left margin.
-    //iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
-    
-    // Changes the desired tail shadow color.
-    //iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': 'rgba(72, 181, 233, 0.6) 0px 1px 6px', 'z-index' : '1'});
-    
-    // Reference to the div that groups the close button elements.
-    //var iwCloseBtn = iwOuter.next();
-    
-    // Apply the desired effect to the close button
-    //iwCloseBtn.css({opacity: '1', right: '38px', top: '3px', border: '7px solid #48b5e9', 'border-radius': '13px', 'box-shadow': '0 0 5px #3990B9'});
-    
-    // If the content of infowindow not exceed the set maximum height, then the gradient is removed.
-    //if($('.iw-content').height() < 140){
-    //$('.iw-bottom-gradient').css({display: 'none'});
-    //}
-    
-    // The API automatically applies 0.7 opacity to the button after the mouseout event. This function reverses this event to the desired value.
-    //iwCloseBtn.mouseout(function(){
-    //$(this).css({opacity: '1'});
-    //});
-    });
-    
+        });
     }
-//google.maps.event.addDomListener(window, 'load', initMap());
 
 
 //burgermenu 
